4134082e1f113c491a8240c20d10629e4a2539df
Sara Song <hyunesong@gmail.com>
2025-08-20 15:25:12 -0700

    Create new project folder for Rock Paper Scissors

---
+++ rock-paper-scissors/rock-paper-scissors.cjs
@@ -0,0 +1,12 @@
+// Set Up 
+const readline = require('readline-sync');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+
+// Rock, Paper, and Scissors
+// 2 player
+
+// If player a chooses rock 
